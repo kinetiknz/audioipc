a4f039cd6e26bf1c10866dab21df20508905019c
Matthew Gregan <kinetik@flim.org>
2023-03-15 16:36:13 +1300

    wip

--- audioipc/src/rpccore.rs
+++ audioipc/src/rpccore.rs
@@ -9,9 +9,8 @@ use std::cell::UnsafeCell;
 use std::collections::VecDeque;
 use std::io::{self, Error, ErrorKind, Result};
 use std::marker::PhantomPinned;
-use std::mem::ManuallyDrop;
 use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
-use std::sync::{Arc, Weak};
+use std::sync::Arc;
 
 use crate::ipccore::EventLoopHandle;
 
@@ -74,27 +73,18 @@ struct CompletionWriter<T> {
 }
 
 impl<T> CompletionWriter<T> {
-    fn set(self, value: T) {
+    fn set(self, value: Option<T>) {
         // Store the result into the Completion's memory.
         // Since `set` consumes `self`, rely on `Drop` to
         // mark the writer as done and wake the Completion's
         // thread.
         unsafe {
             assert!((*self.ptr).writer.load(Ordering::Relaxed));
-            *(*self.ptr).item.get() = Some(value);
-        }
-    }
-}
-
-impl<T> Drop for CompletionWriter<T> {
-    fn drop(&mut self) {
-        // Mark writer as complete - if `set` was not called,
-        // the waiter will receive `None`.
-        unsafe {
+            *(*self.ptr).item.get() = value;
             (*self.ptr).writer.store(false, Ordering::Release);
+            // Wake the Completion's thread.
+            self.waiter.unpark();
         }
-        // Wake the Completion's thread.
-        self.waiter.unpark();
     }
 }
 
@@ -149,39 +139,30 @@ type ProxyRequest<Request, Response> = (Request, CompletionWriter<Response>);
 #[derive(Debug)]
 pub struct Proxy<Request, Response> {
     handle: Option<(EventLoopHandle, Token)>,
-    requests: ManuallyDrop<Weak<RequestQueue<Request, Response>>>,
+    requests: Arc<RequestQueue<Request, Response>>,
 }
 
 impl<Request, Response> Proxy<Request, Response> {
-    fn new(requests: Weak<RequestQueue<Request, Response>>) -> Self {
-        if let Some(req) = requests.upgrade() {
-            // Handles case of cloning a disconnected/dead proxy
-            // Since upgrade failed here, it will also fail
-            // for any subsequent call() to this new dead proxy.
-            req.attach_proxy();
-        }
+    fn new(requests: Arc<RequestQueue<Request, Response>>) -> Self {
+        requests.attach_proxy();
         Self {
             handle: None,
-            requests: ManuallyDrop::new(requests),
+            requests,
         }
     }
 
     pub fn call(&self, request: Request) -> Result<Response> {
         let response = Completion::new();
-        {
-            let req = if let Some(req) = self.requests.upgrade() {
-                req
-            } else {
-                debug!("Proxy[{:p}]: call failed - CH::requests dropped", self);
-                return Err(Error::new(ErrorKind::Other, "proxy send error"));
-            };
-            let response_writer = response.writer();
-            if req.push((request, response_writer)).is_err() {
-                debug!("Proxy[{:p}]: call failed - CH::requests full", self);
-                return Err(Error::new(ErrorKind::Other, "proxy send error"));
-            }
+        if self.requests.push((request, response.writer())).is_err() {
+            debug!("Proxy[{:p}]: call failed - CH::requests full", self);
+            std::mem::forget(response); // avoid waiting inside drop
+            return Err(Error::new(ErrorKind::Other, "proxy send error"));
         }
         self.wake_connection();
+        if self.requests.server_connected.load(Ordering::Acquire) == SERVER_DISCONNECTED {
+            std::mem::forget(response); // avoid waiting inside drop
+            return Err(Error::new(ErrorKind::Other, "server disconnected during call - proxy send error"));
+        }
         match response.wait() {
             Some(resp) => Ok(resp),
             None => Err(Error::new(ErrorKind::Other, "proxy recv error")),
@@ -203,7 +184,7 @@ impl<Request, Response> Proxy<Request, Response> {
 
 impl<Request, Response> Clone for Proxy<Request, Response> {
     fn clone(&self) -> Self {
-        let mut clone = Self::new((*self.requests).clone());
+        let mut clone = Self::new(self.requests.clone());
         let (handle, token) = self
             .handle
             .as_ref()
@@ -219,14 +200,9 @@ impl<Request, Response> Drop for Proxy<Request, Response> {
         // Must drop `requests` before waking the connection, otherwise
         // the wake may be processed before the (last) weak reference is
         // dropped.
-        if let Some(req) = self.requests.upgrade() {
-            let last = req.detach_proxy();
-            unsafe {
-                ManuallyDrop::drop(&mut self.requests);
-            }
-            if last && self.handle.is_some() {
-                self.wake_connection()
-            }
+        let last = self.requests.detach_proxy();
+        if last && self.handle.is_some() {
+            self.wake_connection()
         }
     }
 }
@@ -261,7 +237,7 @@ impl<C: Client> Handler for ClientHandler<C> {
     fn consume(&mut self, response: Self::In) -> Result<()> {
         trace!("ClientHandler::consume");
         if let Some(response_writer) = self.in_flight.pop_front() {
-            response_writer.set(response);
+            response_writer.set(Some(response));
         } else {
             return Err(Error::new(ErrorKind::Other, "request/response mismatch"));
         }
@@ -298,34 +274,50 @@ impl<C: Client> Drop for ClientHandler<C> {
     fn drop(&mut self) {
         self.requests
             .server_connected
-            .store(false, Ordering::Relaxed);
+            .store(SERVER_DISCONNECTING, Ordering::Release);
+
+        // Unblock all in-flight requests.
+        while let Some(writer) = self.in_flight.pop_front() {
+            writer.set(None);
+        }
+        while let Some((_, writer)) = self.requests.pop() {
+            writer.set(None);
+        }
+        
+        self.requests.server_connected.store(SERVER_DISCONNECTED, Ordering::Release);
     }
 }
 
 #[derive(Debug)]
 struct RequestQueue<Request, Response> {
     requests: ArrayQueue<ProxyRequest<Request, Response>>,
-    server_connected: AtomicBool,
+    server_connected: AtomicUsize,
     proxies_connected: AtomicUsize,
 }
+
+const SERVER_DISCONNECTED: usize = 0;
+const SERVER_CONNECTED: usize = 1;
+const SERVER_DISCONNECTING: usize = 2;
 
 impl<Request, Response> RequestQueue<Request, Response> {
     fn new() -> Self {
         RequestQueue {
             requests: ArrayQueue::new(RPC_CLIENT_INITIAL_PROXIES),
-            server_connected: AtomicBool::new(true),
+            server_connected: AtomicUsize::new(SERVER_CONNECTED),
             proxies_connected: AtomicUsize::new(0),
         }
     }
 
     fn push(&self, request: ProxyRequest<Request, Response>) -> Result<()> {
+        if self.server_connected.load(Ordering::Acquire) != SERVER_CONNECTED {
+            debug!("RequestQueue[{:p}]: push failed - server not connected", self);
+            return Err(Error::new(ErrorKind::Other, "server not connected"));
+        }
+        // Caller is committed to waiting on Completion once this push succeeds.
         if self.requests.push(request).is_err() {
+            debug!("RequestQueue[{:p}]: push failed - queue full", self);
             return Err(Error::new(ErrorKind::Other, "queue full"));
         }
-        // XXX need to remove request or force a wait
-        // if !self.server_connected.load(Ordering::Relaxed) {
-        //     return Err(Error::new(ErrorKind::Other, "server not connected"));
-        // }
         Ok(())
     }
 
@@ -334,8 +326,6 @@ impl<Request, Response> RequestQueue<Request, Response> {
     }
 
     fn attach_proxy(&self) {
-        // attaching to a disconnected server is ok - call() will fail
-        //assert!(self.server_connected.load(Ordering::Relaxed));
         self.proxies_connected.fetch_add(1, Ordering::Relaxed);
     }
 
@@ -350,7 +340,7 @@ impl<Request, Response> RequestQueue<Request, Response> {
 pub(crate) fn make_client<C: Client>(
 ) -> Result<(ClientHandler<C>, Proxy<C::ServerMessage, C::ClientMessage>)> {
     let requests = Arc::new(RequestQueue::new());
-    let proxy_req = Arc::downgrade(&requests);
+    let proxy_req = requests.clone();
     let handler = ClientHandler::new(requests);
     let proxy = Proxy::new(proxy_req);
     Ok((handler, proxy))
